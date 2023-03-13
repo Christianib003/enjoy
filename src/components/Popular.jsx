7fe4a35e9488a9d5e2edbdaeb9320bac4185278a
Christian <christianib003@gmail.com>
2023-03-13 10:59:14 +0200

    add gradient

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -20,17 +20,33 @@ function Popular() {
 
   return (
     <div>
-      {popular.map((recipe) => (
-        <Wrapper key={recipe.id}>
-          <Card>
-            <p>{recipe.title}</p>
-            <img
-              src={recipe.image}
-              alt={recipe.title}
-            />
-          </Card>
-        </Wrapper>
-      ))}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem',
+          }}
+        >
+          {popular.map((recipe) => (
+            <>
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img
+                    src={recipe.image}
+                    alt={recipe.title}
+                  />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            </>
+          ))}
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
@@ -40,13 +56,44 @@ const Wrapper = styled.div`
 `;
 
 const Card = styled.div`
-  min-height: 25rem;
+  min-height: 15rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
