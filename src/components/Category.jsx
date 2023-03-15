b1d9b5e087d81723489c348b233cf724f347cd07
Christian <christianib003@gmail.com>
2023-03-15 11:42:01 +0200

    change divs to react links

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,26 +1,27 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
 import styled from 'styled-components';
+import { NavLink, NavLinkProps } from 'react-router-dom';
 
 function Category() {
   return (
     <List>
-      <div>
+      <NavLink>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink>
         <FaHamburger />
         <h4>American</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink>
         <GiNoodles />
         <h4>Thai</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </div>
+      </NavLink>
     </List>
   );
 }
